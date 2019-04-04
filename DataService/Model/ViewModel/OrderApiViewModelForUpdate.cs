9176ea7076f61f69c0b39d3eb0c0b50e54310401
Anh <anh.nguyen@wisky.vn>
2019-04-04 15:56:46 +0700

    04/04/2019

--- DataService/Model/ViewModel/OrderApiViewModelForUpdate.cs
+++ DataService/Model/ViewModel/OrderApiViewModelForUpdate.cs
@@ -8,7 +8,8 @@ namespace DataService.Model.ViewModel
 {
     public class OrderApiViewModelForUpdate
     {
-        //public int OrderId { get; set; }
+        [Required]
+        public int RentID { get; set; }
         public string InvoiceID { get; set; }
         public System.DateTime CheckInDate { get; set; }
         public System.DateTime CheckOutDate { get; set; }
@@ -51,19 +52,94 @@ namespace DataService.Model.ViewModel
         public int GroupPaymentStatus { get; set; }
 
         //public int StoreId { get; set; }
-        public List<OrderDetailApiViewModel> OrderDetailViewModels { get; set; }
-        public List<PaymentApiViewModel> PaymentMs { get; set; }
-        public List<OrderPromotionMappingModel> OrderPromotionMappingMs { get; set; }
+        public List<OrderDetailApiViewModelForUpdate> OrderDetailViewModels { get; set; }
+        public List<PaymentApiViewModelForUpdate> PaymentMs { get; set; }
+        public List<OrderPromotionMappingModelForUpdate> OrderPromotionMappingMs { get; set; }
         public Nullable<System.DateTime> LastModifiedPayment { get; set; }
         //{ get return DateTime.Now; set; }
         public Nullable<System.DateTime> LastModifiedOrderDetail { get; set; }
 
         public OrderApiViewModelForUpdate()
         {
-            OrderDetailViewModels = new List<OrderDetailApiViewModel>();
-            PaymentMs = new List<PaymentApiViewModel>();
-            OrderPromotionMappingMs = new List<OrderPromotionMappingModel>();
+            OrderDetailViewModels = new List<OrderDetailApiViewModelForUpdate>();
+            PaymentMs = new List<PaymentApiViewModelForUpdate>();
+            OrderPromotionMappingMs = new List<OrderPromotionMappingModelForUpdate>();
         }
 
+    }
+    public class OrderDetailApiViewModelForUpdate
+    {
+        public OrderDetailApiViewModelForUpdate()
+        {
+            OrderDetailPromotionMappingMs = new List<OrderDetailPromotionMappingModelForUpdate>();
+        }
+        public int OrderDetailID { get; set; }
+        //public int RentId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public double FinalAmount { get; set; }
+        public double TotalAmount { get; set; }
+        public double Discount { get; set; }
+        public int Quantity { get; set; }
+        public System.DateTime OrderDate { get; set; }
+        public int Status { get; set; }
+        public string Notes { get; set; }
+        public bool isAddition { get; set; }
+        public int TaxValue { get; set; }
+        public double UnitPrice { get; set; }
+        public int ProductType { get; set; }
+        public int? ParentId { get; set; }
+        public int ProductOrderType { get; set; }
+        public int ItemQuantity { get; set; }
+        //public int StoreId { get; set; }
+        public int? TmpDetailId { get; set; }
+        public List<OrderDetailPromotionMappingModelForUpdate> OrderDetailPromotionMappingMs { get; set; }
+        //public OrderPromotionMappingModel OrderPromotionMapping { get; set; }
+        //public OrderDetailPromotionMappingModel OrderDetailPromotionMapping { get; set; }
+        public Nullable<int> OrderPromotionMappingId { get; set; }
+        public Nullable<int> OrderDetailPromotionMappingId { get; set; }
+    }
+
+    public class PaymentApiViewModelForUpdate
+    {
+        public int PaymentID { get; set; }
+        //public int RentId { get; set; }
+        public int Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal FCAmount { get; set; }
+        public string Notes { get; set; }
+        public System.DateTime PayTime { get; set; }
+        public int Status { get; set; }
+        public int Type { get; set; }
+        public string CardCode { get; set; }
+    }
+
+    public class OrderPromotionMappingModelForUpdate
+    {
+        public int Id { get; set; }
+        public int RentId { get; set; }
+        [JsonProperty("PromotionId")]
+        public string PromotionCode { get; set; }
+        [JsonProperty("PromotionDetailId")]
+        public string PromotionDetailCode { get; set; }
+        public Nullable<decimal> DiscountAmount { get; set; }
+        public Nullable<int> DiscountRate { get; set; }
+        public int MappingIndex { get; set; }
+        public Nullable<int> TmpMappingId { get; set; }
+    }
+
+    public class OrderDetailPromotionMappingModelForUpdate
+    {
+        public int Id { get; set; }
+        public int OrderDetailId { get; set; }
+        [JsonProperty("PromotionId")]
+        public string PromotionCode { get; set; }
+        [JsonProperty("PromotionDetailId")]
+        public string PromotionDetailCode { get; set; }
+        public Nullable<decimal> DiscountAmount { get; set; }
+        public Nullable<int> DiscountRate { get; set; }
+        public int MappingIndex { get; set; }
+        public Nullable<int> TmpMappingId { get; set; }
     }
 }
--- DataService/ServiceAPI/OrderService.cs
+++ DataService/ServiceAPI/OrderService.cs
@@ -21,6 +21,7 @@ namespace DataService.ServiceAPI
         //OrderViewModel GetOrderByRentID(int storeId, int rentId);
         OrderViewModel GetOrderByRentID(OrderRequestModel model);
         bool CreateOrder(int storeId, OrderApiViewModel orderRequest);
+        bool UpdateOrder(int storeId, OrderApiViewModelForUpdate orderRequest);
     }
 
     public class OrderService : BaseService<Order, OrderViewModel>, IOrderService
@@ -104,6 +105,7 @@ namespace DataService.ServiceAPI
                 var orderDetail = new OrderDetailViewModel();
                 MapOrderDetail(orderDetailRequest, orderDetail, storeId);
                 orderDetail.RentId = rId;
+                orderDetail.IsAddition = false;
                 orderDetail.Active = true;
                 _orderDetailService.Create(orderDetail);
             }
@@ -122,41 +124,84 @@ namespace DataService.ServiceAPI
             return true;
         }
 
-        public bool UpdateOrder(int storeId, OrderApiViewModel orderRequest)
+        public bool UpdateOrder(int storeId, OrderApiViewModelForUpdate orderRequest)
         {
             var _orderDetailService = ServiceFactory.CreateService<IOrderDetailService>(_serviceProvider);
             var _paymentService = ServiceFactory.CreateService<IPaymentService>(_serviceProvider);
             var order = new OrderViewModel();
-            MapOrder(orderRequest, order, storeId);
-            //if (orderRequest.)
-            this.Update(order);
-            var rId = this.LastOrDefault().RentId;
-            foreach (var orderDetailRequest in orderRequest.OrderDetailViewModels)
-            {
-                var orderDetail = new OrderDetailViewModel();
-                MapOrderDetail(orderDetailRequest, orderDetail, storeId);
-                orderDetail.RentId = rId;
-                orderDetail.Active = true;
-                _orderDetailService.Create(orderDetail);
-            }
             if (orderRequest.PaymentMs.Count > 0)
             {
                 foreach (var paymentRequest in orderRequest.PaymentMs)
                 {
                     var payment = new PaymentViewModel();
-                    MapPayment(paymentRequest, payment);
-                    payment.ToRentId = rId;
+                    MapPaymentForUpdate(paymentRequest, payment);
+                    payment.ToRentId = orderRequest.RentID;
                     //payment = true;
-                    _paymentService.Create(payment);
+                    _paymentService.Update(payment);
                 }
                 //orderRequest.Room.CurrentRentId = -1;
             }
+            foreach (var orderDetailRequest in orderRequest.OrderDetailViewModels)
+            {
+                var orderDetail = new OrderDetailViewModel();
+                MapOrderDetailForUpdate(orderDetailRequest, orderDetail, storeId);
+                orderDetail.RentId = orderRequest.RentID;
+                orderDetail.Active = true;
+                _orderDetailService.Update(orderDetail);
+            }
+            MapOrderForUpdate(orderRequest, order, storeId);
+            //if (orderRequest.)
+            this.Update(order);
             return true;
         }
 
         private void MapOrder(OrderApiViewModel source, OrderViewModel destination, int storeId)
         {
             //destination.RentId = source.OrderId;
+            destination.OrderStatus = source.OrderStatus;
+            destination.CheckInPerson = source.CheckInPerson;
+            destination.DeliveryStatus = source.DeliveryStatus;
+            destination.InvoiceId = source.InvoiceID;
+            //destination.CheckInDate = source.CheckInDate;
+            destination.CheckInDate = DateTime.UtcNow;
+            destination.CheckOutDate = DateTime.UtcNow;      //TODO: change
+            destination.ApproveDate = DateTime.UtcNow;       //TODO: change
+            destination.TotalAmount = source.TotalAmount;
+            destination.Discount = source.Discount;
+            destination.DiscountOrderDetail = source.DiscountOrderDetail;
+            destination.FinalAmount = source.FinalAmount;
+            destination.OrderType = source.OrderType;
+            destination.Notes = source.Notes;
+            destination.FeeDescription = source.FeeDescription;
+            destination.CheckOutPerson = source.CheckOutPerson;
+            destination.ApprovePerson = source.ApprovePerson;
+            if (source.CustomerID != 0)
+                destination.CustomerId = source.CustomerID;
+            destination.IsFixedPrice = source.IsFixedPrice;
+            destination.ServedPerson = source.ServedPerson;
+            destination.StoreId = storeId;
+            destination.SourceId = source.SourceID;
+            destination.SourceType = 0;
+            destination.DeliveryAddress = source.DeliveryAddress;
+            destination.TotalInvoicePrint = source.TotalInvoicePrint;
+            destination.Vat = source.VAT;
+            destination.Vatamount = source.VATAmount;
+            destination.NumberOfGuest = source.NumberOfGuest;
+            destination.Att1 = source.Att1;
+            destination.Att2 = source.Att2;
+            destination.Att3 = source.Att3;
+            destination.Att4 = source.Att4;
+            destination.Att5 = source.Att5;
+            destination.GroupPaymentStatus = source.GroupPaymentStatus;
+            destination.DeliveryReceiver = source.DeliveryCustomer;
+            destination.DeliveryPhone = source.DeliveryPhone;
+            destination.LastModifiedPayment = source.LastModifiedPayment;
+            destination.LastModifiedOrderDetail = source.LastModifiedOrderDetail;
+        }
+
+        private void MapOrderForUpdate(OrderApiViewModelForUpdate source, OrderViewModel destination, int storeId)
+        {
+            destination.RentId = source.RentID;
             destination.OrderStatus = source.OrderStatus;
             destination.CheckInPerson = source.CheckInPerson;
             destination.DeliveryStatus = source.DeliveryStatus;
@@ -223,11 +268,51 @@ namespace DataService.ServiceAPI
             destination.OrderDetailPromotionMappingId = source.OrderDetailPromotionMappingId;
         }
 
+        private void MapOrderDetailForUpdate(OrderDetailApiViewModelForUpdate source, OrderDetailViewModel destination, int storeId)
+        {
+            destination.OrderDetailId = source.OrderDetailID;
+            destination.ProductId = source.ProductId;
+            destination.StoreId = storeId;
+            destination.TotalAmount = source.TotalAmount;
+            destination.Quantity = source.Quantity;
+            //destination.OrderDate = source.OrderDate;
+            destination.OrderDate = DateTime.UtcNow;
+            destination.Status = source.Status;
+            if (source.isAddition != null)
+            {
+                destination.IsAddition = source.isAddition;
+            } else destination.IsAddition = false;
+            destination.FinalAmount = source.FinalAmount;
+            destination.Discount = source.Discount;
+            destination.TaxValue = source.TaxValue;
+            destination.UnitPrice = source.UnitPrice;
+            destination.ProductType = source.ProductType;
+            destination.ParentId = source.ParentId;
+            destination.ProductOrderType = source.ProductOrderType;
+            destination.ItemQuantity = source.ItemQuantity;
+            destination.TmpDetailId = source.TmpDetailId;
+            destination.OrderPromotionMappingId = source.OrderPromotionMappingId;
+            destination.OrderDetailPromotionMappingId = source.OrderDetailPromotionMappingId;
+        }
+
         /// <summary>
         /// Map payment data form source(model) to destination
         /// </summary>
         private void MapPayment(PaymentApiViewModel source, PaymentViewModel destination)
         {
+            destination.Amount = source.Amount;
+            destination.CurrencyCode = source.CurrencyCode;
+            destination.Fcamount = source.FCAmount;
+            destination.Notes = source.Notes;
+            destination.PayTime = DateTime.UtcNow;
+            destination.Status = source.Status;
+            destination.Type = source.Type;
+            destination.CardCode = source.CardCode;
+        }
+
+        private void MapPaymentForUpdate(PaymentApiViewModelForUpdate source, PaymentViewModel destination)
+        {
+            destination.PaymentId = source.PaymentID;
             destination.Amount = source.Amount;
             destination.CurrencyCode = source.CurrencyCode;
             destination.Fcamount = source.FCAmount;
